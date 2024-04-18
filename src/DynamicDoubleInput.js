96b8c606f853aa684f3288a3a185e75f45fed24d
Rayan Hossain <80379977+rayan2228@users.noreply.github.com>
2024-04-18 22:45:44 +0600

    package updated dynamic double input  added

---
+++ src/DynamicDoubleInput.js
@@ -0,0 +1,91 @@
+const DynamicDoubleInput = ({
+  length = null,
+  cancelClassName = null,
+  inputClassName = null,
+  buttonClassName = null,
+  inputAttributes = null,
+  inputOptions,
+  setInputOptions,
+  containerClassName = null,
+  itemClassName = null,
+  buttonAttributes = null,
+  buttonText = "add",
+  buttonError = null,
+  cancelIcon = null,
+  buttonIconBefore = null,
+  buttonIconAfter = null,
+}) => {
+  const handleInputChange = (index, field, event) => {
+    const newInputs = [...inputOptions];
+    newInputs[index][field] = event.target.value;
+    setInputOptions(newInputs);
+  };
+  const handleAddInput = () => {
+    if (length) {
+      if (inputOptions.length < length) {
+        const newId = inputOptions.length + 1;
+        setInputOptions([
+          ...inputOptions,
+          { id: newId, value1: "", value2: "" },
+        ]);
+      }
+    } else {
+      const newId = inputOptions.length + 1;
+      setInputOptions([...inputOptions, { id: newId, value1: "", value2: "" }]);
+    }
+  };
+  const handleDeleteOption = (index) => {
+    const newInputs = [...inputOptions];
+    newInputs.splice(index, 1);
+    setInputOptions(newInputs);
+  };
+  return (
+    <div className={`containerCss2 ${containerClassName}`}>
+      {inputOptions.length > 0 &&
+        inputOptions.map((inputValue, index) => (
+          <div key={index} className={`itemCss2 ${itemClassName}`}>
+            <input
+              type="text"
+              value={inputValue.value1}
+              onChange={(event) => handleInputChange(index, "value1", event)}
+              className={`inputCss2  ${inputClassName}`}
+              {...inputAttributes}
+            />
+            <input
+              type="text"
+              value={inputValue.value2}
+              onChange={(event) => handleInputChange(index, "value2", event)}
+              className={`inputCss2  ${inputClassName}`}
+              {...inputAttributes}
+            />
+            <span
+              onClick={() => handleDeleteOption(index)}
+              className={`crossCss2 ${cancelClassName}`}
+            >
+              <img src={cancelIcon} alt={cancelIcon} loading="lazy" />
+            </span>
+          </div>
+        ))}
+      <button
+        onClick={handleAddInput}
+        type="button"
+        className={`buttonCss2 ${buttonClassName}  ${length &&
+          inputOptions.length === length &&
+          (buttonError || "errorCss2")
+          } `}
+        disabled={length && inputOptions.length === length ? true : false}
+        {...buttonAttributes}
+      >
+        {buttonIconBefore && (
+          <img src={buttonIconBefore} alt={cancelIcon} loading="lazy" />
+        )}
+        {buttonText}
+        {buttonIconAfter && (
+          <img src={buttonIconAfter} alt={cancelIcon} loading="lazy" />
+        )}
+      </button>
+    </div>
+  );
+};
+
+export default DynamicDoubleInput;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 export { default as DynamicInput } from './DynamicInput';
+export { default as DynamicDoubleInput } from './DynamicDoubleInput';
